0724fcafd2556eb083f4a2054e42ca3c67301db7
Vishnu S A <vishnulyrics@gmail.com>
2022-09-02 22:06:25 +0530

    angularweb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { response } from 'express';
+import { AppServiceService } from './app-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-app';
+
+  constructor(private service : AppServiceService){
+
+  }
+
+  ngOnInit(){
+this.getdataFromAPI();
+  }
+ getdataFromAPI(){
+   this.service.getData().subscribe((response) => {
+    console.log('API connected',response)
+
+   },(error)=>{
+    console.log("not connected",error)
+   })
+}
 }
